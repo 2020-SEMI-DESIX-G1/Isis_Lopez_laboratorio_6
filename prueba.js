9510013efc909a25fbaefe2a72668fe4f2e665ac
Your Name <i.loopez12@gmail.com>
2020-06-23 04:35:01 +0000

    2

--- prueba.js
+++ prueba.js
@@ -7,11 +7,11 @@ let primera = 'HOLA';
 let segunda = ' MUNDO'
 
 
-console.log('Punto A Var: SUMA: '+(a+b)+' RESTA: '+(a-b)+' DIVISION: '+(a/b)+' MULTIPLICACION: '+(a*b));
-console.log('Punto B Let:'+primera+segunda);
+console.log('Punto A Var: SUMA: '+a+' + '+b+' = '+(a+b)+' RESTA: '+a+' - '+b+' = '+(a-b)+' DIVISION: '+a+' / '+b+' = '+(a/b)+' MULTIPLICACION: '+a+' * '+b+' = '+(a*b));
+console.log('Punto B Let: '+primera+segunda);
 
 const tipo= true;
-console.log('Punto C CONST:'+typeof(tipo));
+console.log('Punto C CONST=> Const Tipo:'+typeof(tipo));
 
 var objeto = new Object();
  objeto.entero = 12;
@@ -19,6 +19,6 @@ var objeto = new Object();
  objeto.Bool = false;
  objeto.vacio;   
 
- console.log('Punto D Objeto: '+objeto.vacio);
+ console.log('Punto D Objeto: '+objeto.Bool);
 
 
