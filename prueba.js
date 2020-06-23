d272a665fa8ed049aafd116db1f03bf8a1e9be6a
Your Name <i.loopez12@gmail.com>
2020-06-23 00:41:49 +0000

    first commit

---
+++ prueba.js
@@ -0,0 +1,23 @@
+
+
+var a ;
+var b ;
+
+let primera = 'HOLA';
+let segunda = ' MUNDO'
+
+console.log('SUMA: '+(a+b)+' RESTA: '+(a-b)+' DIVISION: '+(a/b)+' MULTIPLICACION: '+(a*b));
+console.log(primera+segunda);
+
+const tipo= true;
+console.log(typeof(tipo));
+
+var objeto = new Object();
+ objeto.entero = 1;
+ objeto.string = 'STRING';
+ objeto.Bool = false;
+ objeto.vac  ;   
+
+ console.log(objeto.vac);
+
+
